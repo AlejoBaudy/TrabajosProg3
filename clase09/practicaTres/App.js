e88e166cc571281cc1b5beff7140234f11446c3d
mi usuario de github <alejobaudy@gmail.com>
2025-10-19 13:14:33 -0300

    Hecha parte de la practica tres

---
+++ clase09/practicaTres/App.js
@@ -0,0 +1,18 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+export default function App() {
+  return (
+    <View >
+      <Text>Probando FlatLists</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+      flat: {
+        fontWeight: "bold",
+      }
+})
+
+
+
